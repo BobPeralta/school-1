a4e51dfb743c10b9b4cad76290479cc99633d11f
Smile4Blitz <66698210+Smile4Blitz@users.noreply.github.com>
2022-11-01 19:11:56 +0100

    Update lab2_2.sh

--- Cloud Fundamentals/Scripts/lab2_2.sh
+++ Cloud Fundamentals/Scripts/lab2_2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # You need an S3-bucket and an EC2-instance with access to the S3-bucket (IAM)
+# website files need to be on the S3-bucket
 # you need an EFS files system which you can access from the EC2 (VPC / subnet / security groups)
 
 # install and update
@@ -7,8 +8,8 @@ sudo apt -y update;
 sudo apt -y install nginx awscli php8.1-fpm;
 
 # install awscli
-sudo aws s3 cp s3://arnohalsberghe/index.php /var/www/html/index.php;
-sudo aws s3 cp s3://arnohalsberghe/default /etc/nginx/sites-available/default;
+sudo aws s3 cp s3://bucketname/index.php /var/www/html/index.php;
+sudo aws s3 cp s3://bucketname/default /etc/nginx/sites-available/default;
 
 sudo systemctl enable nginx;
 sudo systemctl restart nginx;
