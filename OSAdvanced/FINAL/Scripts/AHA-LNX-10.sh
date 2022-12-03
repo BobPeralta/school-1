6740e33a3bc26a8ab8f0c19186272b0247e12f1a
Smile4Blitz <arnohalsberghe@outlook.com>
2022-12-03 01:52:33 +0100

    lnx-10

---
+++ OSAdvanced/FINAL/Scripts/AHA-LNX-10.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+# static ip (not sure if ip forwarding gets enabled):
+#     /etc/sysconfig/network/ifroute-eth0
+#     /etc/sysconfig/network/ifcfg-eth0
+#     /etc/sysconfig/network/ifroute-eth1
+#     /etc/sysconfig/network/ifcfg-eth1
+#     /etc/sysconfig/network/routes
+echo "AHA-LNX-10" > "/etc/hostname";
+echo "nameserver 10.11.8.10" > "/etc/resolv.conf";
+
+# disable firewall:
+sudo systemctl disable firewalld;
+sudo systemctl stop firewalld;
--- OSAdvanced/FINAL/Scripts/AHA-LNX-DMZ.sh
+++ OSAdvanced/FINAL/Scripts/AHA-LNX-DMZ.sh
@@ -3,6 +3,7 @@
 #     /etc/sysconfig/network/ifroute-eth0
 #     /etc/sysconfig/network/ifcfg-eth0
 #     /etc/resolv.conf
+echo "AHA-LNX-DMZ" > "/etc/hostname";
 
 # disable firewall:
 sudo systemctl disable firewalld;
@@ -12,19 +13,19 @@ sudo systemctl stop firewalld;
 zypper -n install nginx unzip rsyslog;
 
 # website
-sudo rm /srv/www/htdocs/* -R;
-sudo rm /tmp* -R;
+sudo rm "/srv/www/htdocs/*" -R;
+sudo rm "/tmp/*" -R;
 
 sudo wget https://github.com/Smile4Blitz/school/raw/main/OSAdvanced/FINAL/Website/FINAL.zip -P /tmp;
-sudo unzip /tmp/FINAL.zip;
+sudo unzip "/tmp/FINAL.zip";
 
-sudo mv /tmp/* /srv/www/htdocs;
-chmod -R 777 /srv/www/htdocs;
+sudo mv "/tmp/*" "/srv/www/htdocs";
+chmod -R 777 "/srv/www/htdocs";
 
 # rsyslog setup
 {
     echo -e "*.* @10.11.8.5"
-} >> /etc/rsyslog.d/remote.conf
+} >> "/etc/rsyslog.d/remote.conf"
 
 # enabling extra packages
 systemctl enable nginx;
