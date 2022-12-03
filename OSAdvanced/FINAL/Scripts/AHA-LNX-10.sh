3650c24fd399b796c23f9e2254262c5730cbe6a1
Smile4Blitz <arnohalsberghe@outlook.com>
2022-12-03 01:59:34 +0100

    Update AHA-LNX-10.sh

--- OSAdvanced/FINAL/Scripts/AHA-LNX-10.sh
+++ OSAdvanced/FINAL/Scripts/AHA-LNX-10.sh
@@ -1,12 +1,23 @@
 #/bin/bash
 # static ip (not sure if ip forwarding gets enabled):
 {
-    echo -e ""
-}
-#     /etc/sysconfig/network/ifroute-eth0
-#     /etc/sysconfig/network/ifcfg-eth0
-#     /etc/sysconfig/network/ifroute-eth1
-#     /etc/sysconfig/network/ifcfg-eth1
+    echo -e "IPADDR='10.11.8.8/24'
+BOOTPROTO='static'
+STARTMODE='hotplug'"
+} > /etc/sysconfig/network/ifcfg-eth0
+
+{
+    echo -e "IPADDR='10.12.8.8/24'
+BOOTPROTO='static'
+STARTMODE='hotplug'"
+} > /etc/sysconfig/network/ifcfg-eth1
+
+echo -e "default 10.11.8.1 - eth0" > /etc/sysconfig/network/ifroute-eth0;
+echo -e "default 10.11.8.1 - eth0" > /etc/sysconfig/network/ifroute-eth0.YaST2save;
+echo -e "10.99.8.0/24 10.12.8.9 - eth1" > /etc/sysconfig/network/ifroute-eth1;
+echo -e "10.99.8.0/24 10.12.8.9 - eth1" > /etc/sysconfig/network/ifroute-eth1.YaST2save;
+echo -e "10.99.8.0       10.12.8.9       255.255.255.0   eth1" > /etc/sysconfig/network/routes.YaST2save;
+
 #     /etc/sysconfig/network/routes
 echo "AHA-LNX-10" > "/etc/hostname";
 echo "nameserver 10.11.8.10" > "/etc/resolv.conf";
