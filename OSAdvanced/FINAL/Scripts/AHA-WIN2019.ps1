f0f4a879cfa75c85603406c29e3ec196cfbb7c99
Smile4Blitz <arnohalsberghe@outlook.com>
2022-12-03 16:01:10 +0100

    potential final update

--- OSAdvanced/FINAL/Scripts/AHA-WIN2019.ps1
+++ OSAdvanced/FINAL/Scripts/AHA-WIN2019.ps1
@@ -1,4 +1,3 @@
-# static IP
 # set computername (requires restart)
 $HOSTNAME=hostname
 $HOSTNAMETARGET="AHA-WIN2019"
@@ -8,18 +7,18 @@ if ($HOSTNAME -ne $HOSTNAMETARGET)
 	Restart-Computer
 	exit
 }
-else {continue}
 
 # set static IP
 New-NetIPAddress -IPAddress 10.11.8.10 -InterfaceAlias "Ethernet" -DefaultGateway 10.11.8.10 -AddressFamily IPv4 -PrefixLength 24
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 127.0.0.1
 
 # DHCP server
-# wget "dhcpexport.xml"
-# install dhcpserver package
-Import-DhcpServer -File "dhcpexport.xml"
+Invoke-WebRequest "https://raw.githubusercontent.com/Smile4Blitz/school/main/OSAdvanced/FINAL/WinSer/dhcpexport.xml" -OutFile "%temp%\dhcpexport.xml"
+Install-WindowsFeature DHCP
+Import-DhcpServer -File "%temp%\dhcpexport.xml"
 
 # DNS server
-# wget "dnsexport.xml"
-$x = Import-Clixml "c:\DnsServerConfig.xml"
+Invoke-WebRequest "https://raw.githubusercontent.com/Smile4Blitz/school/main/OSAdvanced/FINAL/WinSer/dnsexport.xml" -OutFile "%temp%\dnsexport.xml"
+Install-WindowsFeature DNS
+$x = Import-Clixml "%temp%\dnsexport.xml"
 Set-DnsServer -InputObject $x -ComputerName "AHA-WIN2019"
