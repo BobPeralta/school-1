4e66fe17e8c1d75a9d4c4d0a8c1ad29413e9fd03
Smile4Blitz <66698210+Smile4Blitz@users.noreply.github.com>
2022-12-07 10:58:39 +0100

    WIN SER final script

--- OSAdvanced/FINAL/Scripts/AHA-WIN2019.ps1
+++ OSAdvanced/FINAL/Scripts/AHA-WIN2019.ps1
@@ -1,24 +1,60 @@
-# set computername (requires restart)
-$HOSTNAME=hostname
-$HOSTNAMETARGET="AHA-WIN2019"
-if ($HOSTNAME -ne $HOSTNAMETARGET)
+Echo "Starting"
+$HostName = hostname
+if ($HostName -ne "AHA-WIN2019")
 {
-	Rename-Computer -Name AHA-LNX-DMZ
-	Restart-Computer
+    Echo "We first have to set your hostname correctly."
+	Rename-Computer -NewName AHA-WIN2019 -Confirm
+    Echo "Restart required, PC will restart in 5 seconds, then run the script again."
+    Start-Sleep -Seconds 5
+    Restart-Computer
 	exit
 }
 
-# set static IP
-New-NetIPAddress -IPAddress 10.11.8.10 -InterfaceAlias "Ethernet" -DefaultGateway 10.11.8.10 -AddressFamily IPv4 -PrefixLength 24
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 127.0.0.1
+if ((Test-Path -Path "C:\Program Files\Mozilla Firefox\Firefox.exe") -eq $false)
+{
+    Echo "Installing Firefox"
+    Invoke-WebRequest "https://download-installer.cdn.mozilla.net/pub/firefox/releases/107.0.1/win64/en-US/Firefox%20Setup%20107.0.1.msi" -OutFile "$env:temp\firefox.msi"
+    msiexec.exe /I "$env:temp\firefox.msi" /quiet
+}
+
+# Set static IP
+Echo "Setting static IP"
+
+Remove-NetIPAddress -InterfaceAlias "Ethernet0" -Confirm:$false -ErrorAction SilentlyContinue 
+Remove-NetRoute -InterfaceAlias "Ethernet0" -Confirm:$false  -ErrorAction SilentlyContinue
+
+New-NetIPAddress -IPAddress 10.11.8.10 -InterfaceAlias "Ethernet0" -DefaultGateway 10.11.8.8 -AddressFamily IPv4 -PrefixLength 24
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddresses 8.8.8.8
+
+# Letting the Windows server load the network settings properly (throws error on hostname lookup later)
+Echo "Loading network settings"
+Start-Sleep -Seconds 5
 
 # DHCP server
-Invoke-WebRequest "https://raw.githubusercontent.com/Smile4Blitz/school/main/OSAdvanced/FINAL/WinSer/dhcpexport.xml" -OutFile "%temp%\dhcpexport.xml"
-Install-WindowsFeature DHCP
-Import-DhcpServer -File "%temp%\dhcpexport.xml"
+Echo "Setting up the DHCP server"
+Invoke-WebRequest "https://raw.githubusercontent.com/Smile4Blitz/school/main/OSAdvanced/FINAL/WinSer/dhcpexport.xml" -OutFile "$env:temp\dhcpexport.xml"
+Install-WindowsFeature DHCP -IncludeManagementTools
+Import-DhcpServer -Confirm:$false -File "$env:temp\dhcpexport.xml" -BackupPath "C:\dhcpbackup"
 
 # DNS server
-Invoke-WebRequest "https://raw.githubusercontent.com/Smile4Blitz/school/main/OSAdvanced/FINAL/WinSer/dnsexport.xml" -OutFile "%temp%\dnsexport.xml"
-Install-WindowsFeature DNS
-$x = Import-Clixml "%temp%\dnsexport.xml"
-Set-DnsServer -InputObject $x -ComputerName "AHA-WIN2019"
+Echo "`nSetting up the DNS server"
+Install-WindowsFeature DNS -IncludeManagementTools
+
+Add-DnsServerPrimaryZone -Name aha.local -DynamicUpdate NonsecureAndSecure -Confirm -ZoneFile "aha.local.dns"
+Add-DnsServerPrimaryZone -NetworkId 10.11.8.0/24 -DynamicUpdate NonsecureAndSecure -ZoneFile "8.11.10.in-addr.arpa.dns"
+Add-DnsServerPrimaryZone -NetworkId 10.12.8.0/24 -DynamicUpdate NonsecureAndSecure -ZoneFile "8.12.10.in-addr.arpa.dns"
+Add-DnsServerPrimaryZone -NetworkId 10.99.8.0/24 -DynamicUpdate NonsecureAndSecure -ZoneFile "8.99.10.in-addr.arpa.dns"
+
+# Cleanup
+Echo "`nCleaning up"
+Remove-Item "$env:temp\dhcpexport.xml"
+if ((Test-Path -Path "C:\Program Files\Mozilla Firefox\Firefox.exe") -eq $false)
+{
+    Remove-Item "$env:temp\firefox.msi"
+}
+
+Echo "Done, PC will restart in 5 seconds."
+Start-Sleep -Seconds 5
+Reboot
+exit
+
