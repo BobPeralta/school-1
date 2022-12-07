0b122a5b743c0c8a088770a2416285edbbb50b5c
Smile4Blitz <66698210+Smile4Blitz@users.noreply.github.com>
2022-12-07 13:19:50 +0100

    Update AHA-WIN2019.ps1

--- OSAdvanced/FINAL/Scripts/AHA-WIN2019.ps1
+++ OSAdvanced/FINAL/Scripts/AHA-WIN2019.ps1
@@ -39,11 +39,17 @@ Add-DnsServerPrimaryZone -NetworkId 10.12.8.0/24 -DynamicUpdate NonsecureAndSecu
 Add-DnsServerPrimaryZone -NetworkId 10.99.8.0/24 -DynamicUpdate NonsecureAndSecure -ZoneFile "8.99.10.in-addr.arpa.dns"
 
 # Installing Firefox
-if ((Test-Path -Path "C:\Program Files\Mozilla Firefox\Firefox.exe") -eq $false)
-{
-    Echo "`nInstalling Firefox"
-    Invoke-WebRequest "https://download-installer.cdn.mozilla.net/pub/firefox/releases/107.0.1/win64/en-US/Firefox%20Setup%20107.0.1.msi" -OutFile "$env:temp\firefox.msi"
-    msiexec.exe /I "$env:temp\firefox.msi" /quiet
+$regKey = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion"
+$Core = (Get-ItemProperty $regKey).InstallationType -eq "Server Core"
+
+if ($Core -eq $False) {
+    
+    if ((Test-Path -Path "C:\Program Files\Mozilla Firefox\Firefox.exe") -eq $false)
+    {
+        Echo "`nInstalling Firefox"
+        Invoke-WebRequest "https://download-installer.cdn.mozilla.net/pub/firefox/releases/107.0.1/win64/en-US/Firefox%20Setup%20107.0.1.msi" -OutFile "$env:temp\firefox.msi"
+        msiexec.exe /I "$env:temp\firefox.msi" /quiet
+    }
 }
 
 # Cleanup
